a62669868fa0fa914cb17f8062c585676b183165
said-yolcu <saidyolcu.saidyolcu@gmail.com>
2025-01-02 16:35:50 +0300

    Add user register and login

---
+++ Services/IAuthenticationService.cs
@@ -0,0 +1,10 @@
+using System.IdentityModel.Tokens.Jwt;
+using AuthenticationApi.Dtos;
+
+namespace AuthenticationApi.Services;
+
+public interface IAuthenticationService
+{
+    Task<string> Register(RegisterRequest request);
+    Task<string> Login(LoginRequest request);
+}
