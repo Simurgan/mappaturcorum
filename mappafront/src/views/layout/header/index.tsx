6fdfab21b72f32d460dfb8462f9f66e35fe91b15
Burak Kızılay <burak.kizilay@westerops.com>
2025-01-20 17:02:28 +0300

    feat: add header

--- mappafront/src/views/layout/header/index.tsx
+++ mappafront/src/views/layout/header/index.tsx
@@ -1,6 +1,47 @@
-import "./style.scss";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+import MappaLogo from "@/assets/images/mappa-logo.png";
+import searchIcon from "@/assets/icons/search.svg";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="border-t-[25px] border-[#B22B24] h-[167px] bg-[#FFFAEA] absolute top-0 left-0 right-0">
+      <div className="flex w-full justify-between px-[192px] pt-[40px]">
+        <div>
+          <img src={MappaLogo} width={205} height={86} />
+        </div>
+        <div className="flex gap-8">
+          <div className="relative h-[30px]">
+            <Input
+              placeholder="Search"
+              className="border-[#B22B24] rounded-3xl h-[30px]"
+            />
+            <img
+              src={searchIcon}
+              width={20}
+              height={20}
+              className="absolute bottom-[4px] right-3"
+            />
+          </div>
+          <div className="flex gap-8">
+            <Button
+              variant={"default"}
+              className="w-[110px] h-[30px]  text-white rounded-3xl"
+            >
+              Sign Up
+            </Button>
+          </div>
+        </div>
+      </div>
+      <nav className="flex justify-end gap-[41px] pr-[192px] text-[#B22B24] text-[21px] font-medium leading-[24px]">
+        <p>Home</p>
+        <p>About</p>
+        <p>People</p>
+        <p>Publications</p>
+        <p>Contant</p>
+      </nav>
+    </header>
+  );
 };
 export default Header;
