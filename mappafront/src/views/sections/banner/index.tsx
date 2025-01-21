d70026ce2691aaad1b9241b2110bcb51b6eff217
Simurgan <uyduranomersukru@gmail.com>
2025-01-21 16:52:56 +0300

    implement banner section

--- mappafront/src/views/sections/banner/index.tsx
+++ mappafront/src/views/sections/banner/index.tsx
@@ -3,11 +3,21 @@ import bgImage from "@/assets/images/katalan-atlasi.jpg";
 const BannerSection = () => {
   return (
     <section
-      style={{ backgroundImage: `url(${bgImage})` }}
+      style={{
+        backgroundImage: `url(${bgImage})`,
+      }}
       // className="bg-[length:100%_100%]"
-      className="section bg-cover bg-center"
+      className="section bg-cover bg-center bg-gradient-to-l from-white"
     >
-      <div className="container"></div>
+      <div className="container">
+        <div className="banner-text">
+          <h1 className="welcome-text text-primary text-[40px] font-light">
+            Welcome to <br />{" "}
+            <span className="font-bold">MAPPA ANATOLICORUM</span> <br />
+            Explore the 1200-1450 Anatolia
+          </h1>
+        </div>
+      </div>
     </section>
   );
 };
