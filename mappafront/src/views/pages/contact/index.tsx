1afb68da787724c67f7996313c6faccf4aedf721
Burak Kızılay <burak.kizilay@westerops.com>
2025-01-23 15:54:01 +0300

    feat: add icons and done page

--- mappafront/src/views/pages/contact/index.tsx
+++ mappafront/src/views/pages/contact/index.tsx
@@ -1,6 +1,10 @@
 import Text from "@/views/components/text";
-import "./style.scss";
 import { Link } from "react-router-dom";
+
+import mailIcon from "@/assets/icons/mail-icon.svg";
+import phoneIcon from "@/assets/icons/phone-icon.svg";
+
+import "./style.scss";
 
 const ContactPage = () => {
   return (
@@ -10,17 +14,23 @@ const ContactPage = () => {
           <Text fs={36} fw={700} lh={125} color="burgundy">
             Contact
           </Text>
-          <Link
-            to={"mailto:info@mappaanatolicum.com"}
-            className="contact-email"
-          >
+          <div className="contact-info">
+            <img src={mailIcon} width={24} height={18} />
+            <Link
+              to={"mailto:info@mappaanatolicum.com"}
+              className="contact-email"
+            >
+              <Text fs={24} fw={400} lh={125} color="burgundy">
+                info@mappaanatolicum.com
+              </Text>
+            </Link>
+          </div>
+          <div className="contact-info">
+            <img src={phoneIcon} width={24} height={18} />
             <Text fs={24} fw={400} lh={125} color="burgundy">
-              info@mappaanatolicum.com
+              +90 123 456 78 90
             </Text>
-          </Link>
-          <Text fs={24} fw={400} lh={125} color="burgundy">
-            +90 123 456 78 90
-          </Text>
+          </div>
         </div>
         <div className="contact-form">
           <Text fs={28} fw={700} lh={125} color="burgundy">
@@ -47,7 +57,7 @@ const ContactPage = () => {
                 Message
               </Text>
             </label>
-            <textarea className="form-input" id="message" />
+            <textarea className="form-input text-area" id="message" />
           </div>
         </div>
       </div>
