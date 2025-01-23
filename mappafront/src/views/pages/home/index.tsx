5e5fea199a5ed375035ad279ff0b811962579f0f
Simurgan <uyduranomersukru@gmail.com>
2025-01-23 18:41:24 +0300

    feat: complete home page

--- mappafront/src/views/pages/home/index.tsx
+++ mappafront/src/views/pages/home/index.tsx
@@ -3,10 +3,24 @@ import BannerSection from "@/views/sections/banner";
 import DataCardsSection from "@/views/sections/data-cards";
 import HomeAboutSection from "@/views/sections/home-about";
 import ToolCardsSection from "@/views/sections/tool-cards";
+import "./style.scss";
 
 const HomePage = () => {
   return (
-    <section className="page-content">
+    <section
+      className="home-page-content"
+      style={{
+        background: `linear-gradient(
+                180deg,
+              #FFF9E9 85%,
+              rgba(255, 249, 233, 0.650485) 92%,
+              rgba(255, 249, 233, 0) 105%
+              ), url(${require("@/assets/images/motif.jpg")})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "contain",
+        backgroundPosition: "0 60%",
+      }}
+    >
       <BannerSection />
       <HomeAboutSection />
       <DataCardsSection />
