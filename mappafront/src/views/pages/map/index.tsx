4d91e633b9b11699a2559c9b6d82cc9d2ecdcd69
Simurgan <uyduranomersukru@gmail.com>
2025-01-24 17:30:03 +0300

    feat: implement some map page layout

--- mappafront/src/views/pages/map/index.tsx
+++ mappafront/src/views/pages/map/index.tsx
@@ -4,17 +4,28 @@ import "./style.scss";
 
 const MapPage = () => {
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
-    </MapContainer>
+    <section className="section map-section">
+      <div className="container">
+        <div className="map-container">
+          <MapContainer
+            center={[51.505, -0.09]}
+            zoom={13}
+            scrollWheelZoom={false}
+          >
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[51.505, -0.09]}>
+              <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+              </Popup>
+            </Marker>
+          </MapContainer>
+        </div>
+        <div className="map-tools-container"></div>
+      </div>
+    </section>
   );
 };
 export default MapPage;
