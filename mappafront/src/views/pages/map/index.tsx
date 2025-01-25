23bd9e6df5a180521409ea3874fd1c63af5f172d
Simurgan <uyduranomersukru@gmail.com>
2025-01-25 16:11:04 +0300

    feat: implement map layout

--- mappafront/src/views/pages/map/index.tsx
+++ mappafront/src/views/pages/map/index.tsx
@@ -1,22 +1,29 @@
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import "./style.scss";
+import L from "leaflet";
 
 const MapPage = () => {
   return (
     <section className="section map-section">
       <div className="container">
         <div className="map-container">
-          <MapContainer
-            center={[51.505, -0.09]}
-            zoom={13}
-            scrollWheelZoom={false}
-          >
+          <MapContainer center={[39.83, 34.96]} zoom={6} scrollWheelZoom={true}>
             <TileLayer
               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker position={[51.505, -0.09]}>
+            <Marker
+              position={[37.0691417, 36.2660925]}
+              // icon={
+              //   new L.Icon({
+              //     iconUrl: require(""),
+              //     iconRetinaUrl: "",
+              //     iconSize: new L.Point(60, 75),
+              //     className: "leaflet-div-icon",
+              //   })
+              // }
+            >
               <Popup>
                 A pretty CSS3 popup. <br /> Easily customizable.
               </Popup>
