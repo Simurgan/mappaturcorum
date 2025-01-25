cff27684ed70d9a88385701f412eecd9a1221520
Simurgan <uyduranomersukru@gmail.com>
2025-01-25 17:58:13 +0300

    feat: change site title and add markers to map

--- mappafront/src/views/pages/map/index.tsx
+++ mappafront/src/views/pages/map/index.tsx
@@ -15,31 +15,6 @@ const MapPage = () => {
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
             <Marker
-              position={[37.0419171, 36.2287413]}
-              icon={
-                new L.Icon({
-                  iconUrl: require("@/assets/icons/blue-marker.svg"),
-                  iconRetinaUrl: require("@/assets/icons/blue-marker.svg"),
-                  iconSize: new L.Point(20, 20),
-                  className: "leaflet-div-icon",
-                })
-              }
-              eventHandlers={{
-                mouseover: (event) => {
-                  event.target.openPopup();
-                },
-                mouseout: (event) => {
-                  event.target.closePopup();
-                },
-              }}
-            >
-              <Popup>
-                <Text tag="p" fw={300} fs={12} lh={125} color="black">
-                  Fakıuşağı, Osmaniye
-                </Text>
-              </Popup>
-            </Marker>
-            <Marker
               position={[38.247275, 36.8946894]}
               icon={
                 new L.Icon({
@@ -61,6 +36,56 @@ const MapPage = () => {
               <Popup>
                 <Text tag="p" fw={300} fs={12} lh={125} color="black">
                   Afşin, Kahramanmaraş
+                </Text>
+              </Popup>
+            </Marker>
+            <Marker
+              position={[37.0572583, 36.1664172]}
+              icon={
+                new L.Icon({
+                  iconUrl: require("@/assets/icons/blue-marker.svg"),
+                  iconRetinaUrl: require("@/assets/icons/blue-marker.svg"),
+                  iconSize: new L.Point(15, 15),
+                  className: "leaflet-div-icon",
+                })
+              }
+              eventHandlers={{
+                mouseover: (event) => {
+                  event.target.openPopup();
+                },
+                mouseout: (event) => {
+                  event.target.closePopup();
+                },
+              }}
+            >
+              <Popup>
+                <Text tag="p" fw={300} fs={12} lh={125} color="black">
+                  Kumarlı, Toprakkale
+                </Text>
+              </Popup>
+            </Marker>
+            <Marker
+              position={[37.0419171, 36.2287413]}
+              icon={
+                new L.Icon({
+                  iconUrl: require("@/assets/icons/blue-marker.svg"),
+                  iconRetinaUrl: require("@/assets/icons/blue-marker.svg"),
+                  iconSize: new L.Point(20, 20),
+                  className: "leaflet-div-icon",
+                })
+              }
+              eventHandlers={{
+                mouseover: (event) => {
+                  event.target.openPopup();
+                },
+                mouseout: (event) => {
+                  event.target.closePopup();
+                },
+              }}
+            >
+              <Popup>
+                <Text tag="p" fw={300} fs={12} lh={125} color="black">
+                  Fakıuşağı, Osmaniye
                 </Text>
               </Popup>
             </Marker>
