3f8859961dfe42bdba3d219295518f1cfbf8c949
Burak Kızılay <burak.kizilay@westerops.com>
2025-01-30 17:35:27 +0300

    feat: add ordinary page action header

--- mappafront/src/views/pages/ordinary-people/index.tsx
+++ mappafront/src/views/pages/ordinary-people/index.tsx
@@ -1,4 +1,35 @@
+import Text from "@/views/components/text";
+import "./style.scss";
+import Button from "@/views/components/button";
+
 const OrdinaryPeoplePage = () => {
-  return <div>OrdinaryPeoplePage</div>;
+  return (
+    <section className="section ordinary-section">
+      <div className="container">
+        <div className="page-head">
+          <Text fs={36} fw={700} lh={125} color="papirus">
+            Unordinary People <br />
+            <Text fs={22} fw={300} lh={125} color="papirus">
+              in the context of their relationship with ordinary people
+            </Text>
+          </Text>
+
+          <div className="page-head-actions-container">
+            <Button classNames="action-button">
+              <Text fs={20} fw={500} lh={125} color="burgundy">
+                Map
+              </Text>
+            </Button>
+            <Button classNames="action-button">
+              <Text fs={20} fw={500} lh={125} color="burgundy">
+                Social Network
+              </Text>
+            </Button>
+            <input placeholder="Search on map.." className="action-input" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 export default OrdinaryPeoplePage;
