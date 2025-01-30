f5643513455c9f4dd6a8d60503cebb18c3947af9
Burak Kızılay <burak.kizilay@westerops.com>
2025-01-30 16:39:43 +0300

    feat: add react-modal package & modal component for map modal

--- mappafront/src/views/pages/map/index.tsx
+++ mappafront/src/views/pages/map/index.tsx
@@ -3,67 +3,49 @@ import "leaflet/dist/leaflet.css";
 import "./style.scss";
 import L from "leaflet";
 import Text from "@/views/components/text";
+import React, { useState } from "react";
+import Button from "@/views/components/button";
+import ReactModal from "react-modal";
+
+ReactModal.setAppElement("#root"); // For blocking not working modal styles in some browsers.
 
 const MapPage = () => {
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+  const [selectedMarker, setSelectedMarker] = useState<string>();
+
+  function openModal(markerValue: string) {
+    setSelectedMarker(markerValue);
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {}
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
   return (
     <section className="section map-section">
       <div className="container">
+        <ReactModal
+          isOpen={modalIsOpen}
+          onAfterOpen={afterOpenModal}
+          onRequestClose={closeModal}
+          contentLabel="Example Modal"
+          overlayClassName="custom-overlay"
+          className="custom-modal"
+        >
+          <div className="modal-content">
+            {selectedMarker}
+            <Button onClick={closeModal}>X</Button>
+          </div>
+        </ReactModal>
         <div className="map-container">
           <MapContainer center={[39.83, 34.96]} zoom={6} scrollWheelZoom={true}>
             <TileLayer
               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker
-              position={[38.247275, 36.8946894]}
-              icon={
-                new L.Icon({
-                  iconUrl: require("@/assets/icons/blue-marker.svg"),
-                  iconRetinaUrl: require("@/assets/icons/blue-marker.svg"),
-                  iconSize: new L.Point(20, 20),
-                  className: "leaflet-div-icon",
-                })
-              }
-              eventHandlers={{
-                mouseover: (event) => {
-                  event.target.openPopup();
-                },
-                mouseout: (event) => {
-                  event.target.closePopup();
-                },
-              }}
-            >
-              <Popup>
-                <Text tag="p" fw={300} fs={12} lh={125} color="black">
-                  Afşin, Kahramanmaraş
-                </Text>
-              </Popup>
-            </Marker>
-            <Marker
-              position={[37.0572583, 36.1664172]}
-              icon={
-                new L.Icon({
-                  iconUrl: require("@/assets/icons/blue-marker.svg"),
-                  iconRetinaUrl: require("@/assets/icons/blue-marker.svg"),
-                  iconSize: new L.Point(15, 15),
-                  className: "leaflet-div-icon",
-                })
-              }
-              eventHandlers={{
-                mouseover: (event) => {
-                  event.target.openPopup();
-                },
-                mouseout: (event) => {
-                  event.target.closePopup();
-                },
-              }}
-            >
-              <Popup>
-                <Text tag="p" fw={300} fs={12} lh={125} color="black">
-                  Kumarlı, Toprakkale
-                </Text>
-              </Popup>
-            </Marker>
             <Marker
               position={[37.0419171, 36.2287413]}
               icon={
@@ -75,6 +57,7 @@ const MapPage = () => {
                 })
               }
               eventHandlers={{
+                click: (event) => openModal(event.target._leaflet_id),
                 mouseover: (event) => {
                   event.target.openPopup();
                 },
