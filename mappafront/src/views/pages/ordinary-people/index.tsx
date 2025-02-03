3ce60029e491bf02ca12cdb51986282ceb678d68
Burak Kızılay <burak.kizilay@westerops.com>
2025-02-03 12:04:48 +0300

    feat: add basic tablecomponent

--- mappafront/src/views/pages/ordinary-people/index.tsx
+++ mappafront/src/views/pages/ordinary-people/index.tsx
@@ -1,6 +1,7 @@
 import Text from "@/views/components/text";
 import "./style.scss";
 import Button from "@/views/components/button";
+import Table from "@/views/components/table";
 
 const OrdinaryPeoplePage = () => {
   return (
@@ -24,6 +25,23 @@ const OrdinaryPeoplePage = () => {
             </Button>
             <input placeholder="Search on map.." className="action-input" />
           </div>
+        </div>
+      </div>
+      <div className="content">
+        <div className="table-container">
+          <Table
+            headers={["ID", "Name"]}
+            data={[
+              { id: 1, name: "Burak", school: "Celal Bayar University" },
+              { id: 2, name: "Ömer", school: "Boğaziçi University" },
+              { id: 3, name: "Burak", school: "Celal Bayar University" },
+              { id: 4, name: "Ömer", school: "Boğaziçi University" },
+              { id: 5, name: "Burak", school: "Celal Bayar University" },
+              { id: 6, name: "Ömer", school: "Boğaziçi University" },
+              { id: 7, name: "Burak", school: "Celal Bayar University" },
+              { id: 8, name: "Ömer", school: "Boğaziçi University" },
+            ]}
+          />
         </div>
       </div>
     </section>
