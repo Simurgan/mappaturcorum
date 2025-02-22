70efccc036042725df93ca48f221e73c9ff810b0
Simurgan <uyduranomersukru@gmail.com>
2025-02-22 18:13:39 +0300

    feat: continue to implement api connection

--- mappafront/src/actions/ordinary.ts
+++ mappafront/src/actions/ordinary.ts
@@ -1,13 +1,16 @@
+import { OrdinaryPageRequestBody } from "@/models/ordinary-people";
 import api from "./api";
 
 const ordinaryApi = api.create({
   baseURL: api.defaults.baseURL + "ordinaryperson/",
 });
 
-export const getOrdinaryPage = async () => {
+export const getOrdinaryPage = async (data: OrdinaryPageRequestBody) => {
   const response = await ordinaryApi({
     url: "page/",
     method: "get",
-    data: 
-  })
-}
+    data: data,
+  });
+
+  return response;
+};
