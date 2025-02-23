aab5b0657b6b3cdb1b6bd54b3d6a0f6a7f9c6625
Simurgan <uyduranomersukru@gmail.com>
2025-02-23 20:00:53 +0300

    remove unnnecessary files

--- mappafront/src/models/ordinary-people.ts
+++ mappafront/src/models/ordinary-people.ts
@@ -1,23 +1,3 @@
-export type OrdinaryPeopleTableDataType = {
-  name: string;
-  alternateName?: string;
-  ethnonym?: string;
-  religion?: string;
-  formerReligion?: string;
-  profession?: string;
-  gender?: "Male" | "Female" | "Other";
-  interestingFeature?: string;
-  interactionOrdinary?: string;
-  interactionOrdinaryExplanation?: string;
-  interactionUnordinary?: string;
-  interactionUnordinaryExplanation?: string;
-  sources?: string[];
-  biography?: string;
-  descriptionInSource?: string;
-  explanationOfEthnicity?: string;
-  version?: string;
-};
-
 export type OrdinaryPageRequestBody = {
   pageSize: number;
   pageNumber: number;
